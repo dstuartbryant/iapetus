db25ece355bfc95c53661f0a3a24e51a196f27c8
Daniel Bryant <dstuartbryant@gmail.com>
2022-08-10 20:31:46 -0700

    continued Vector class dev

--- iapetus/linalg/__init__.py
+++ iapetus/linalg/__init__.py
@@ -1,21 +1,51 @@
 """Linear algebra tools."""
 
-from typing import Union
+from typing import List, Union
 
 import numpy as np
 
 
+class VectorException(Exception):
+    pass
+
+
 class Vector(np.ndarray):
-    def __init__(self, arg: Union[np.ndarray, list]):
+    def __init__(self, arg: Union[np.ndarray, List[float]]):
         if isinstance(arg, np.ndarray):
+            if arg.ndim != 1:
+                raise VectorException(
+                    f"Expecting number of dimensions length of 1, found {arg.ndim}."
+                )
             self._vector = arg
         elif isinstance(arg, list):
             self._vector = np.array(arg)
-
-        raise NotImplementedError("Need to handle vector shape constraints.")
+            if len([x for x in arg if isinstance(x, list)]) != 0:
+                raise VectorException("Not expecting nested lists.")
+        # raise NotImplementedError("Need to handle vector shape constraints.")
 
     def __str__(self):
         return str(self._vector)
 
     def __repr__(self):
         return repr(self._vector)
+
+    def _dim(self):
+        return self._vector.shape[0]
+
+    def _mag(self):
+        return np.linalg.norm(self._vector)
+
+    def _unit(self):
+        return self._vector / self._mag()
+
+    @property
+    def dim(self):
+        return self._dim()
+
+    @property
+    def mag(self):
+        return self._mag()
+
+    @property
+    def unit(self):
+        return self._unit()
